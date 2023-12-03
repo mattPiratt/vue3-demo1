ae83e01f1eac377e4d96f8041239600939a84e55
Bartosz Kubek <bartosz.kubek@gmail.com>
2023-12-03 11:07:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <h1>The App</h1>
+    <ul>
+        <learning-resource-item 
+        v-for="resourceItem in storredResources" 
+        v-bind="resourceItem" 
+        :key="resourceItem.id"
+        >
+        </learning-resource-item>
+    </ul>
+</template>
+
+<script>
+import LearningResourceItem from './components/LerningResource/Item.vue';
+
+export default {
+    components: {
+        LearningResourceItem,
+    },
+    data() {
+        return {
+            storredResources: [
+                {
+                    id: 'res1',
+                    title: 'Official giude',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org',
+                },
+                {
+                    id: 'res2',
+                    title: 'Google',
+                    description: 'You must know how to google',
+                    link: 'https://google.com',
+                },
+            ],
+        }
+    }
+}
+</script>
